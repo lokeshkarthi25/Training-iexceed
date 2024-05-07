f9f031fa8d8ce7bc93aa323acf06a6bfe98ce38c
lokeshkarthi25 <150770253+lokeshkarthi25@users.noreply.github.com>
2024-05-07 08:07:13 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -2,23 +2,23 @@ public class sample{
     sample(byte x){
             if(x>0){
                 if(x%2==0){
-                    System.out.println(x+"is postive and even")
+                    System.out.println(x+"is postive and even");
                 }
                 else{
-                    System.out.println(x+"is postive and odd")
+                    System.out.println(x+"is postive and odd");
 
                 }
             }
             else if(x<0){
-                System.out.println(x+"is negative ")
+                System.out.println(x+"is negative ");
             }
             else{
-                System.out.println(x+"is Zero")
+                System.out.println(x+"is Zero");
             }
     }
 }
 public class demo{
     public static void main(String args[]){
-        new sample(5)
+        new sample(5);
     }
 }
