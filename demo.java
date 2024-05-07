268c8d77a20814e4390219e030f25c033dd5bed4
lokeshkarthi25 <150770253+lokeshkarthi25@users.noreply.github.com>
2024-05-07 08:06:15 +0530

    Add files via upload
    day1

---
+++ demo.java
@@ -0,0 +1,24 @@
+public class sample{
+    sample(byte x){
+            if(x>0){
+                if(x%2==0){
+                    System.out.println(x+"is postive and even")
+                }
+                else{
+                    System.out.println(x+"is postive and odd")
+
+                }
+            }
+            else if(x<0){
+                System.out.println(x+"is negative ")
+            }
+            else{
+                System.out.println(x+"is Zero")
+            }
+    }
+}
+public class demo{
+    public static void main(String args[]){
+        new sample(5)
+    }
+}
