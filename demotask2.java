ee57796410d81b07c8cef8fa66dfdc6629be7568
lokeshkarthi25 <150770253+lokeshkarthi25@users.noreply.github.com>
2024-05-09 09:37:02 +0530

    day

--- demotask2.java
+++ demotask2.java
@@ -5,14 +5,13 @@ class Arithmetic{
    
 }
 class Adder extends Arithmetic{
-      String getClass(String w){
-        return w;
-     }
+     
+     
 }
 public class demotask2 {
         public static void main(String []args){
             Adder a = new Adder();
-            System.out.println("My superclass is: " + a.getClass("Arithmetic"));	
+            System.out.println("My superclass is: " + a.getClass().getSuperclass().getName());	
                         System.out.print(a.add(10,32) + " " + a.add(10,3) + " " + a.add(10,10) + "\n");
          }
     }
