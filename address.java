27823b8d3abd5d6a11a962f6a0b586e12fffe139
lokeshkarthi25 <150770253+lokeshkarthi25@users.noreply.github.com>
2024-05-09 09:16:41 +0530

    day3

---
+++ address.java
@@ -0,0 +1,17 @@
+class address {
+      String st_name,city;
+      int plot_no,pin;
+      address(String st_name,String city,int plot_no,int pin){
+        this.st_name=st_name;
+        this.city=city;
+        this.plot_no=plot_no;
+        this.pin=pin;
+      }
+      public void displayadr(){
+        System.out.println("Street name "+st_name);
+        System.out.println("city name  "+city);
+        System.out.println("Plot no "+plot_no);
+        System.out.println("Pin no"+pin);
+      }
+          
+}
