d81843c4590eea1f883320754e068e7d08a4bb2c
lokeshkarthi25 <150770253+lokeshkarthi25@users.noreply.github.com>
2024-05-14 09:05:26 +0530

    day6

---
+++ demostringliteral.java
@@ -0,0 +1,17 @@
+class literal{
+    literal(){ 
+        String a="played well";
+        String b="keep well";
+       StringBuffer c =new StringBuffer("Dhoni");//this method can be mutable
+       c.append(b);
+       System.out.println(c);
+       
+    }
+}
+
+public class demostringliteral {
+    public static void main(String[] args) {
+     new literal();
+    }
+}
+//op : Dhoni keep well
