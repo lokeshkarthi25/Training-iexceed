98cc907cfa483b0178a3b776827e8b05572d9c88
lokeshkarthi25 <150770253+lokeshkarthi25@users.noreply.github.com>
2024-05-21 09:56:44 +0530

    day13

---
+++ variable2.js
@@ -0,0 +1,22 @@
+// can we change the values of variable inside the block?
+
+function nellai(){
+    var x="alwa"
+    x="thamirabarani"
+    console.log(x);
+}
+nellai()//here we see clearly that we can able to change the var values
+
+function madurai(){
+    let x="temple"
+    x="jigarthanda"
+    console.log(x);
+}
+madurai()//here we see clearly that we can able to change the let values
+
+function kerala(){
+    const x="nature"
+    x="trees"
+    console.log(x);
+}
+kerala()// we can't change the const values
