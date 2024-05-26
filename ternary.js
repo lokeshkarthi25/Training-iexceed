a0d6469269aeea6f7b9b876516c3c2bfa149002c
lokeshkarthi25 <150770253+lokeshkarthi25@users.noreply.github.com>
2024-05-26 15:32:06 +0530

    Delete ternary.js

--- ternary.js
+++
@@ -1,9 +0,0 @@
-
-// using ternary operator to find the largest number
-function largest(x,y,z){
-    let big=(x>y && x>z)?x:(y>x)&&(y>z)?y:(z>x)&&(z>y)?z:"All numbers are Equal"
-    console.log(big);
-}
-largest(33, 5, 8)
-largest(100, 1000, 200)
-largest(30, 30, 30) 
