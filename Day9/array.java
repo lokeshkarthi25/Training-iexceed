e239e383abfac9c889ca1c698d4ccac7e8d38d97
lokeshkarthi25 <150770253+lokeshkarthi25@users.noreply.github.com>
2024-05-27 10:53:56 +0530

    Update array.java

--- Day9/array.java
+++ Day9/array.java
@@ -1,7 +1,15 @@
-package Day9;
+class Main {
 
-public class array {
     public static void main(String[] args) {
-        
+
+        // declare the variable and allocate memory
+        int[][] myArray = new int[4][2];
+        myArray[2][1] = 1;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print(myArray[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
