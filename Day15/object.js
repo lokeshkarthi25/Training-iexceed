33557ab09317fc91a0bb7961b60aafc6a7684653
lokeshkarthi25 <150770253+lokeshkarthi25@users.noreply.github.com>
2024-05-27 10:47:10 +0530

    Update object.js

--- Day15/object.js
+++ Day15/object.js
@@ -1,4 +1,24 @@
-let student={
-    name:"loki",
-    age:9
+let obj1={
+    bat:"sg",
+    ball:"kokubura",
+    players:11,
+    win:true
 }
+console.log(obj1.win);//object literals
+
+obj2=new Object()
+obj2.country="Australia"
+obj2.Pname="maxwell"
+obj2.trophy=6
+
+console.log(obj2.trophy);//object created instance
+
+let student={
+    dob:2002,
+    calculateAge(){
+        console.log(new Date().getFullYear()-this.dob);
+    }
+}
+let student1=student
+student=null
+student1.calculateAge()
