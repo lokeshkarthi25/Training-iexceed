378a6200bd79b411d55c0da4a33a9c41f8567a07
lokeshkarthi25 <150770253+lokeshkarthi25@users.noreply.github.com>
2024-05-29 08:13:38 +0530

    Update object.js

--- Day15/object.js
+++ Day15/object.js
@@ -22,3 +22,20 @@ let student={
 let student1=student
 student=null
 student1.calculateAge()
+
+
+// object creation by constructor method
+function Employee(id,names,salary){
+    this.id=id
+    this.name=names
+    this.salary=salary
+    this.changesalary=changesalary //assign the method in property
+
+    function changesalary(othersalary){
+        this.salary=othersalary
+    }
+}
+const e1=new Employee(10,"loki",20000)
+console.log(e1.name);
+e1.changesalary(23000)
+console.log(e1.salary);
