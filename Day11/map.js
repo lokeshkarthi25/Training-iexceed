7760e52db444e0c923dfe4ee5a025f914b1277fc
lokeshkarthi25 <150770253+lokeshkarthi25@users.noreply.github.com>
2024-05-30 09:44:02 +0530

    Update map.js

--- Day11/map.js
+++ Day11/map.js
@@ -1,1 +1,19 @@
+let numbers=[1,2,3,4,5,6,7,8,9]
+let x=['raja','ravi','santhosh']
+let mymap=new Map()
+mymap.set(1,1200)
+mymap.set(true,'isavailable')
+console.log(mymap.get(1));
+console.log(mymap.get(true));
 
+let myset=new Set()
+
+myset.add('java')
+myset.add('javascript')
+myset.add('python')
+myset.add(24)
+
+for (let i of myset) {
+    console.log(i)
+}
+console.log(myset.has("java"));
