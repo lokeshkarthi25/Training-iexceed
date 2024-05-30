9c474d58591ed8fc26e7ce59594aca3287d63e2f
lokeshkarthi25 <150770253+lokeshkarthi25@users.noreply.github.com>
2024-05-30 09:53:29 +0530

    Create method.js

---
+++ Day1/method.js
@@ -0,0 +1,13 @@
+class sample{
+    public void getdata(String x){
+        
+        System.out.println(x);
+    }
+}
+
+
+class datatype{
+    public static void main(String args[]){
+        new sample().getdata("I'm trainee in iexceed");
+    }
+}
