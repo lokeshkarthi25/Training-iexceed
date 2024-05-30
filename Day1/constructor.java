a9e04cd59f46ddd7b8164a6b3583fd7f6cd1d241
lokeshkarthi25 <150770253+lokeshkarthi25@users.noreply.github.com>
2024-05-30 09:46:41 +0530

    Create constructor.java

---
+++ Day1/constructor.java
@@ -0,0 +1,16 @@
+class sample
+{
+    sample(int x,int y)
+    {
+      System.out.println(x*y);
+    }
+}
+
+public class constructor {
+ 
+    public static void main(String asd[])
+    {
+        sample obj1=new sample(3,6);
+        
+    }
+}//output:18
