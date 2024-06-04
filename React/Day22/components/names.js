dd35f7b05139d86a767f36c2bf224a3fc1c4d327
lokeshkarthi25 <150770253+lokeshkarthi25@users.noreply.github.com>
2024-06-04 09:19:36 +0530

    Create names.js

---
+++ React/Day22/components/names.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Names extends Component{
+    render(){
+        return(
+        <p>we going learn about {this.props.sub}</p>)
+    }
+}
+export default Names
